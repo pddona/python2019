d616c30af79be356ce3bf81583c368e7575519fa
Pddona <pddona@gmail.com>
2019-03-05 11:39:10 +0100

    Create 3numeros.py

---
+++ 3numeros.py
@@ -0,0 +1,16 @@
+#!/usr/bin/bash   pyhton 3.py
+# __*__   coding:utf-8  __*__
+def main():
+    print("ORDENAR 6 NÚMEROS")
+    lista = []
+
+    lista.append( int(input("Introduzca el primer número: ")) )
+    lista.append( int(input("Introduzca el primer número: ")) )
+    lista.append( int(input("Introduzca el primer número: ")) )
+
+    lista.sort()
+
+    print(lista)
+        
+if __name__ == "__main__":
+    main()
